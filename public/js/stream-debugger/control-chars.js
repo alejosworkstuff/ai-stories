c3da76943d7172771f292c2d84c0fba54576e9d9
Alejo Castillo <alejoworkstuff@gmail.com>
2026-08-25 14:36:08 -0300

    chore: stop tracking generated stream-debugger client artifacts

--- public/js/stream-debugger/control-chars.js
+++
@@ -1,4 +0,0 @@
-const unexpectedControlCharsPattern = /[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/;
-export function hasUnexpectedControlChars(value) {
-    return unexpectedControlCharsPattern.test(value);
-}
